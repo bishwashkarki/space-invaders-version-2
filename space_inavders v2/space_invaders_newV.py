e8d235aed382977ebae08b7bf7cedd9f0d1a1f4f
bishwashkarki <93584579+bishwashkarki@users.noreply.github.com>
2022-02-02 16:09:44 -0100

    Add files via upload

---
+++ space_inavders v2/space_invaders_newV.py
@@ -0,0 +1,92 @@
+import pygame
+
+screen = pygame.display.set_mode((1600,900))
+#screen = pygame.display.set_mode((1500,800))
+
+background = pygame.image.load('images/background_space.bmp').convert()
+player1=pygame.image.load('images/player1.bmp').convert()
+projectile1=pygame.image.load('images/fireball1.gif').convert()
+player1 = pygame.transform.scale(player1, (50, 50))
+projectile1 = pygame.transform.scale(projectile1, (20, 20))
+FPS=60
+fpsClock=pygame.time.Clock()
+
+
+
+class space_ships:
+
+    def space_ship_movement():
+        spritex=700
+        spritey=800
+        proj_x=spritex
+        proj_y=spritey
+        speed=5
+
+        WHITE=(255, 255, 255)
+        player1.set_colorkey(WHITE)
+
+        main=True
+
+        while main:
+            screen.blit(background,(0,0))
+            screen.blit(player1, (spritex, spritey))
+
+            
+
+            keys_pressed = pygame.key.get_pressed()
+
+            if keys_pressed[pygame.K_LEFT]:
+                spritex -= speed
+
+            if keys_pressed[pygame.K_RIGHT]:
+                spritex += speed
+
+            if keys_pressed[pygame.K_UP]:
+                spritey -= speed
+
+            if keys_pressed[pygame.K_DOWN]:
+                spritey += speed
+
+            if keys_pressed[pygame.K_z]:
+                screen.blit(projectile1, (proj_x, proj_y))
+                proj_y -= speed
+                pygame.time.delay(100)
+                proj_y -= speed
+                pygame.time.delay(100)
+                proj_y -= speed
+                pygame.time.delay(100)
+
+                proj_y -= speed
+                pygame.time.delay(100)
+                proj_y -= speed
+                pygame.time.delay(100)
+                proj_y -= speed
+                pygame.time.delay(100)
+                proj_y -= speed
+                pygame.time.delay(100)
+                proj_y -= speed
+                pygame.time.delay(100)
+
+                
+                
+                
+
+
+
+
+
+
+            pygame.display.update()
+            fpsClock.tick(FPS)
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit(); 
+                    main = False
+                if event.type == pygame.KEYDOWN:
+                    if event.key == ord('q'):
+                        pygame.quit()
+                        main = False 
+
+        
+
---
+++ space_inavders v2/starter.py
@@ -0,0 +1,18 @@
+#creating an game in which you can control a character and destroy "bots" comming towards you 
+
+
+
+#importing all the modules needed to create this app
+
+import pygame
+import space_invaders_newV
+
+
+
+#setting up pygame
+pygame.init()
+                        
+
+
+space_invaders_newV.space_ships.space_ship_movement()
+#space_ships_main.space_ships.shoot_prjectiles()
